0c52283a7108f58b9b299f4529c67a24e2ca3541
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-06-17 21:11:33 +0300

    Limit wizard movement in game area

--- game-engine.js
+++ game-engine.js
@@ -4,7 +4,7 @@ import { config } from "./game-config.js";
 // Game frames
 function newFrame() {
     modifyWizardPosition();
-    
+
     // Game over check
     if (!state.isGameOver) {
         window.requestAnimationFrame(newFrame);
@@ -13,22 +13,25 @@ function newFrame() {
 
 function modifyWizardPosition() {
     const wizardElement = document.querySelector('.wizard');
+    const gameArea = document.querySelector('.game-area');
+
+    const { wizard } = state;
 
     // Wizard movement
-    if (state.controls.KeyA) {
-        wizardElement.style.left = `${state.wizard.x -= config.speed}px`;
+    if (state.controls.KeyA && wizard.x > 0) {
+        wizardElement.style.left = `${wizard.x -= config.speed}px`;
     }
 
-    if (state.controls.KeyD) {
-        wizardElement.style.left = `${state.wizard.x += config.speed}px`;
+    if (state.controls.KeyD && wizard.x + wizard.width < gameArea.offsetWidth) {
+        wizardElement.style.left = `${wizard.x += config.speed}px`;
     }
 
-    if (state.controls.KeyW) {
-        wizardElement.style.top = `${state.wizard.y -= config.speed}px`;
+    if (state.controls.KeyW && wizard.y > 0) {
+        wizardElement.style.top = `${wizard.y -= config.speed}px`;
     }
 
-    if (state.controls.KeyS) {
-        wizardElement.style.top = `${state.wizard.y += config.speed}px`;
+    if (state.controls.KeyS && wizard.y + wizard.height < gameArea.offsetHeight) {
+        wizardElement.style.top = `${wizard.y += config.speed}px`;
     }
 }
 
